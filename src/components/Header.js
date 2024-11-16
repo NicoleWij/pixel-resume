2ad9069405fffe8e7ef6db46961a630b69d3be6b
NicoleWij <nicole.wijkman@gmail.com>
2024-11-16 03:11:49 +0100

    Added Arrows

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Header.css";
 import cloudImage from "../assets/Cloud.png";
+import arrowImage from "../assets/Arrow.png";
 
 const Header = ({ activeSection }) => {
   const sectionTitles = {
@@ -12,8 +13,14 @@ const Header = ({ activeSection }) => {
 
   return (
     <div className="header-container">
-      <img src={cloudImage} alt="Cloud" className="header-image" />
-      <h1>{sectionTitles[activeSection] || "Nicole's Resume"}</h1>
+      <div className="cloud-box">
+        <img src={cloudImage} alt="Cloud" className="header-image" />
+        <h1>{sectionTitles[activeSection] || "Nicole's Resume"}</h1>
+      </div>
+      <div className="arrow-instruction">
+        <img src={arrowImage} alt="Arrow keys" className="arrow-image" />
+        <span>Use the arrows to navigate!</span>
+      </div>
     </div>
   );
 };
